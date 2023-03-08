308903d9e1ac8af09b695d57edd20f79e5deb136
prolajumokeoni <prolajumokeoni@gmail.com>
2023-03-08 12:16:48 +0100

    Style nft card

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import NFTCard from "./components/nft-card.component";
 
 const App = () => {
   const [NFTs, setNFTs] = useState([]);
-
+  const [walletAddress, setWalletAdress] = useState("0xA858DDc0445d8131daC4d1DE01f834ffcbA52Ef1")
+  
   let requestOptions = {
     method: "GET",
     redirect: "follow",
@@ -11,20 +12,22 @@ const App = () => {
 
   const apiKey = "hI9DDDu2O5rwfGIY5dSAA7SYVUkbzYF2";
   const baseURL = `https://eth-mainnet.g.alchemy.com/nft/v2/${apiKey}/getNFTs/`;
-  const ownerAddr = "0xf54c9A0E44a5F5aFd27C7aC8a176A843b9114F1d";
-  const fetchURL = `${baseURL}?owner=${ownerAddr}`;
+  const fetchURL = `${baseURL}?owner=${walletAddress}`;
+  
   const fetchInfo = async () => {
     return await fetch(fetchURL, requestOptions)
       .then((response) => response.json())
       .then((nfts) => setNFTs(nfts.ownedNfts));
-  };
+    };
   useEffect(() => {
     fetchInfo();
   });
   console.log(NFTs)
+
   return (
     <div>
-      <NFTCard NFTs={NFTs} />
+    <input value={walletAddress} type={"text"} placeholder="Add your wallet address"  onChange={(e)=>{setWalletAdress(e.target.value)}} />
+    <NFTCard NFTs={NFTs} />
     </div>
   );
 };
--- src/components/nft-card.component.jsx
+++ src/components/nft-card.component.jsx
@@ -11,40 +11,68 @@ const NFTCard = ({ NFTs }) => {
   return (
     <div className="nft-container">
       {NFTs.length &&
-        NFTs.map((nft) => {
+        NFTs.map((nft, idx) => {
           return (
-            <div className="nft-card" key={nft.id.tokenId}>
-              <h1>{nft.title}</h1>
-              <div>
+            <div className="nft-card" key={idx}>
+            <div className="img-wrapper">
                 <img
                   src={nft.media[0].gateway}
                   className="nft-img"
                   alt={`nft ${nft.title}`}
                 />
-              </div>
-              <div>
+                </div>
+              <div className="details-wrapper">
+                <h4 className="card-title">{nft.title}</h4>
+                <div className="details-card">
                 <p>Id: {nft.id.tokenId.substr(nft.id.tokenId.length - 4)}</p>
-                <p>{nft.description?.substr(0, 150)}</p>
-                <button onClick={() => popupContent(nft)}>open</button>
+                <button onClick={() => popupContent(nft)} className="nft-button">View more</button>
+                </div>
               </div>
             </div>
-          );
+          )
         })}
-     {toggle &&  <div className="popup-container" onClick={popupContent}>
+      {toggle && (
+        <div className="popup-container" onClick={popupContent}>
           <div className="popup-body" onClick={(e) => e.stopPropagation()}>
-        <div className="popup-header">
-          <button onClick={popupContent}>X</button>
-          </div>
+            <div className="popup-header">
+              <button onClick={popupContent}>X</button>
+            </div>
             {popup.map((pop) => {
               return (
                 <div>
+                  <img
+                    src={pop.media[0].gateway}
+                    className="pop-img"
+                    alt={`nft ${pop.title}`}
+                  />
+                  <div>
                   <h1>{pop.title}</h1>
                   <p>{pop.description}</p>
+                  <div>
+                    <a
+                      target="_blank"
+                      rel="noreferrer"
+                      href={`https://etherscan.io/token/${pop.contract.address}`}
+                      className="py-2 px-4 bg-blue-500 w-1/2 text-center rounded-m text-white cursor-pointer"
+                    >
+                      View on openSea
+                    </a>
+                    <a
+                      target="_blank"
+                      rel="noreferrer"
+                      href={`https://etherscan.io/token/${pop.contract.address}`}
+                      className="py-2 px-4 bg-blue-500 w-1/2 text-center rounded-m text-white cursor-pointer"
+                    >
+                      View on etherscan
+                    </a>
+                  </div>
+                  </div>
                 </div>
               );
             })}
+          </div>
         </div>
-      </div>}
+      )}
     </div>
   );
 };
