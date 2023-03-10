f823778f92c602ba2ad12873e42fa4559437e59c
prolajumokeoni <prolajumokeoni@gmail.com>
2023-03-10 12:29:28 +0100

    Change title to name in cards

--- src/components/nft-card.component.jsx
+++ src/components/nft-card.component.jsx
@@ -1,7 +1,6 @@
 import "./nft-card.styles.css";
 import { useState } from "react";
 
-
 const NFTCard = ({ NFTs }) => {
   const [popup, setPopup] = useState([]);
 
@@ -11,49 +10,47 @@ const NFTCard = ({ NFTs }) => {
     setPopup([nft]);
     setToggle(!toggle);
   };
-
   return (
     <div className="nft-container">
-      { 
-        NFTs.map((nft, idx) => {
-          return (
-            <div className="nft-card" key={idx}>
-              <div className="img-wrapper">
-                {nft.media[0].gateway !== "" ? (
-                  <img
-                    src={nft.media[0].gateway}
-                    className="nft-img"
-                    alt={`nft ${nft.title}`}
-                  />
-                ) : (
-                  <img
-                    src={`https://robohash.org/${idx}?set=set2&size=180x180`}
-                    alt={`nft ${nft.title}`}
-                  />
-                )}
-              </div>
-              <div className="details-wrapper">
-                {nft.title !== "" ? ( 
-                  <h4 className="card-title">{(nft.title.length < 10 ) ? (nft.title) : (nft.title.substr(0, nft.title.length - 5))}</h4>
-                ) : ( <h4 className="card-title">No name yet for</h4> )}
-                <div className="details-card">
-                  <p>Id: {nft.id.tokenId.substr(nft.id.tokenId.length - 4)}</p>
-                  <button
-                    onClick={() => popupContent(nft)}
-                    className="nft-button"
-                  >
-                    View more
-                  </button>
-                </div>
+      {NFTs.map((nft, idx) => {
+        return (
+          <div className="nft-card" key={idx}>
+            <div className="img-wrapper">
+              {nft.media[0].gateway !== "" ? (
+                <img
+                  src={nft.media[0].gateway}
+                  className="nft-img"
+                  alt={`nft ${nft.title}`}
+                />
+              ) : (
+                <img
+                  src={`https://robohash.org/${idx}?set=set2&size=180x180`}
+                  alt={`nft ${nft.title}`}
+                />
+              )}
+            </div>
+            <div className="details-wrapper">
+              <h4 className="card-title">{nft.contractMetadata.name}</h4>
+              <div className="details-card">
+                <p>Id: {nft.id.tokenId.substr(nft.id.tokenId.length - 4)}</p>
+                <button
+                  onClick={() => popupContent(nft)}
+                  className="nft-button"
+                >
+                  View more
+                </button>
               </div>
             </div>
-          );
-        })}
+          </div>
+        );
+      })}
       {toggle && (
         <div className="popup-container" onClick={popupContent}>
           <div className="popup-body" onClick={(e) => e.stopPropagation()}>
             <div className="popup-header">
-              <button className="close" onClick={popupContent}>X</button>
+              <button className="close" onClick={popupContent}>
+                X
+              </button>
             </div>
             {popup.map((pop, idx) => {
               return (
@@ -74,15 +71,7 @@ const NFTCard = ({ NFTs }) => {
                     )}
                   </div>
                   <div className="pop-div">
-                    {pop.title !== "" ? (
-                      <h2>{pop.title}</h2>
-                    ) : (
-                      <h2>
-                        No name yet for{" "}
-                        {pop.id.tokenId.substr(pop.id.tokenId.length - 6)}
-                      </h2>
-                    )}
-
+                    <h2>{pop.contractMetadata.name}</h2>
                     {pop.description !== "" ? (
                       <p>{pop.description}</p>
                     ) : (
@@ -93,12 +82,14 @@ const NFTCard = ({ NFTs }) => {
                         quia vero suscipit dolor, ex enim. Est, ipsa.
                       </p>
                     )}
-                    
+
                     <div className="pop-links">
                       <a
                         target="_blank"
                         rel="noreferrer"
-                        href={`https://opensea.io/assets/${pop.contract.address}/${pop.title.substr(1,pop.title.length-1)}`}
+                        href={`https://opensea.io/assets/${
+                          pop.contract.address
+                        }/${pop.title.substr(1, pop.title.length - 1)}`}
                         className="py-2 px-4 bg-blue-500 w-1/2 text-center rounded-m text-white cursor-pointer"
                       >
                         View on openSea
